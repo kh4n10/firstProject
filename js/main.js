d802a3fc7c44f17fa15bb2e6e45dfbc573e894a2
Abass Khan <abassk2011@hotmail.com>
2017-09-18 13:46:04 +0100

    addition of keydown for keyboard and li elements for grid using jquery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,59 @@
 $(function() {
 	console.log("does this work");
 
+// hide the game at the start, so displays instructions first
+$(".gameDomain, #gameHeading, li, .playerAtom").hide();
 
 
+
+// function to create the gaming board
+function creatingGrid () {
+	for (var i = 0; i < 100; i++) {
+		$(".gameDomain").append("<li></li>");
+	};
+};
+
+creatingGrid ();
+
+// function end
+
+
+ // <div class='playerAtom'></div> 
+
+
+// this one click function allows user to start the game and then hides the instructions and displays the game
 $("#startGame").one("click", function() {
-	$(".cards, #startGame").hide();
+	$(".cards, #startGame, .playerAtom").hide();
+	$(".gameDomain, #gameHeading, li").show();
 });
+
+
+
+// var blocksPerRow = 10;
+
+// $("body").on("keydown", function(e){
+
+//     var thisIndex = $(".selected").index();
+//     var newIndex = null;
+
+//     if(e.keyCode === 38) {
+//        console.log("it went up!");
+//        newIndex = thisIndex - blocksPerRow;
+//        $(".playerAtom").show();
+//     }else if (e.keyCode === 40) {
+//         console.log("it went down");
+//         newIndex = thisIndex + blocksPerRow;       
+//     }else if (e.keyCode === 39) {
+//     	console.log("it went right");
+//     	newIndex = thisIndex + 1;
+//     }else if (e.keyCode === 37) {
+//     	console.log("it went left bruh");
+//     	newIndex = thisIndex - 1; 
+//     }
+//     if(newIndex !== null) { 
+//         $(".test").eq(newIndex).addClass("selected").siblings().removeClass("selected");   
+//     }    
+ // });
 
 
 
